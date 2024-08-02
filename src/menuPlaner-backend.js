abce0317cdad78a530fb5916b046428c009b2d8f
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-08-02 16:16:15 +0200

    first version using a real LLM for the POST response to the frontend

--- src/menuPlaner-backend.js
+++ src/menuPlaner-backend.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors');
+require('dotenv').config();
+// Huggingface LLM Inference Endpoint
+const ENDPOINT = 'https://api-inference.huggingface.co/models/google/flan-t5-large'
 
 app.set('view engine', 'ejs')
 app.use(cors()); // Cross Origin Resource Sharing => needed bc two different portsw
@@ -16,9 +19,30 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.post('/menu', (req, res) => {
+app.post('/menu', async (req, res) => {
     //console.log('This is in the POST: ', req)
     const { title, body, userId } = req.body
     console.log(`Title: ${title}, Body: ${body}, UserId: ${userId}`);
-    res.send('Hello from Backend :)')
+    try {
+        const llm_response = await query(body)
+        console.log('LLM: \n', llm_response)
+        res.send(llm_response)
+        //res.send('Hello from Backend :)')
+    } catch (error) {
+        console.log("ERROR IN LLM FETCH")
+    }
 })
+
+async function query(data) {
+    console.log('data: ', data)
+    const response = await fetch(
+        ENDPOINT,
+        {
+            headers: { Authorization: `Bearer ${process.env.HUGGINGFACE_TOKEN}` },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
