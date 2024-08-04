f5e848137b31d0853d19072e1d9a2bef76d14616
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-08-04 21:10:36 +0200

    working now but need refactoring and cleaning up ASAP

--- src/menuPlaner-backend.js
+++ src/menuPlaner-backend.js
@@ -5,11 +5,13 @@ const port = 3000
 const cors = require('cors');
 require('dotenv').config();
 // Huggingface LLM Inference Endpoint
-const ENDPOINT = 'https://api-inference.huggingface.co/models/google/flan-t5-large'
+const ENDPOINT = 'https://api-inference.huggingface.co/models/mistralai/Mistral-Nemo-Instruct-2407'
 
 app.set('view engine', 'ejs')
 app.use(cors()); // Cross Origin Resource Sharing => needed bc two different portsw
 app.use(express.json())
+// LLM to get things done...
+const HfInference = require('@huggingface/inference').HfInference;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -24,25 +26,26 @@ app.post('/menu', async (req, res) => {
     const { title, body, userId } = req.body
     console.log(`Title: ${title}, Body: ${body}, UserId: ${userId}`);
     try {
-        const llm_response = await query(body)
+        const llm_response = await get_llm_response(body)
         console.log('LLM: \n', llm_response)
         res.send(llm_response)
         //res.send('Hello from Backend :)')
     } catch (error) {
+        console.log(error)
         console.log("ERROR IN LLM FETCH")
     }
 })
 
-async function query(data) {
-    console.log('data: ', data)
-    const response = await fetch(
-        ENDPOINT,
-        {
-            headers: { Authorization: `Bearer ${process.env.HUGGINGFACE_TOKEN}` },
-            method: "POST",
-            body: JSON.stringify(data),
-        }
-    );
-    const result = await response.json();
-    return result;
+
+async function get_llm_response(data) {
+    console.log("data:\n", data)
+    const HF_TOKEN = process.env.HUGGINGFACE_TOKEN
+    const inference = new HfInference(HF_TOKEN);
+    const out = await inference.chatCompletion({
+        model: "mistralai/Mistral-7B-Instruct-v0.2",
+        messages: [{ role: "user", content: data}],
+        max_tokens: 1000
+      });
+    console.log("Out:\n", out.choices[0].message)
+    return out.choices[0].message
 }
