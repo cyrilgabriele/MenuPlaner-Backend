c9f4df3b36764693ba6e1d3bb50915ba6019272a
Cyril Gabriele <GabrieleC@regatron.com>
2024-08-09 10:48:56 +0200

    setup of sample response to not exceed HG rate limit

--- src/menuPlaner-backend.js
+++ src/menuPlaner-backend.js
@@ -4,6 +4,96 @@ import 'dotenv/config'
 import { HfInference } from "@huggingface/inference";
 import cors from 'cors'
 
+const sampleResponse = {
+  role: 'assistant',
+  content: ' {\n' +
+    '\n' +
+    '"Monday_Breakfast": "Oatmeal with Bananas, Walnuts and Honey",\n' +
+    '"Monday_Lunch": "Grilled Chicken Sandwich with Avocado, Lettuce, Tomatoes and Whole Grain Bread",\n' +
+    '"Monday_Dinner": "Ground beef Stir-Fry with Broccoli, Bell Peppers and Brown Rice",\n' +
+    '\n' +
+    '"Tuesday_Breakfast": "Scrambled Eggs with Spinach and Whole Grain Toast",\n' +
+    '"Tuesday_Lunch": "Quinoa Salad with Chickpeas, Cucumber, Tomatoes, Feta Cheese and Lemon Vinaigrette",\n' +     
+    '"Tuesday_Dinner": "Baked Salmon with Asparagus and Sweet Potato",\n' +
+    '\n' +
+    '"Wednesday_Breakfast": "Greek Yogurt with Berries, Granola and Honey",\n' +
+    '"Wednesday_Lunch": "Turkey Wrap with Hummus, Sprouts, Cucumber and Whole Wheat Tortilla",\n' +
+    '"Wednesday_Dinner": "Spaghetti Bolognese with Ground Turkey, Carrots, Celery, Onions and Tomato Sauce",\n' +    
+    '\n' +
+    '"Thursday_Breakfast": "Avocado Toast with Scrambled Eggs and Cherry Tomatoes",\n' +
+    '"Thursday_Lunch": "Grilled Veggie Panini with Mozzarella, Pesto and Ciabatta",\n' +
+    '"Thursday_Dinner": "Grilled Chicken with Green Beans, Roasted Potatoes and Balsamic Glaze",\n' +
+    '\n' +
+    '"Friday_Breakfast": "Fruit Smoothie with Banana, Spinach, Almond Milk and Chia Seeds",\n' +
+    '"Friday_Lunch": "Grilled Cheese Sandwich with Tomato Soup",\n' +
+    '"Friday_Dinner": "Beef Tacos with Black Beans, Corn, Salsa and Brown Rice",\n' +
+    '\n' +
+    '"Saturday_Breakfast": "Scrambled Eggs with Mushrooms and Whole Grain English Muffins",\n' +
+    '"Saturday_Lunch": "BBQ Chicken Salad with Mixed Greens, Corn, Black Beans, Avocado and BBQ Sauce",\n' +
+    '"Saturday_Dinner": "Baked Penne with Meatballs, Tomato Sauce and Mozzarella",\n' +
+    '\n' +
+    '"Sunday_Breakfast": "Pancakes with Blueberries and Maple Syrup",\n' +
+    '"Sunday_Lunch": "Grilled Vegetable Sandwich with Hummus and Whole Grain Bread",\n' +
+    '"Sunday_Dinner": "Roasted Chicken with Carrots, Parsnips, and Green Beans",\n' +
+    '\n' +
+    '"Ingredients": [\n' +
+    '"Ground Beef",\n' +
+    '"Broccoli",\n' +
+    '"Bell Peppers",\n' +
+    '"Brown Rice",\n' +
+    '"Oats",\n' +
+    '"Bananas",\n' +
+    '"Walnuts",\n' +
+    '"Honey",\n' +
+    '"Chicken Breasts",\n' +
+    '"Avocado",\n' +
+    '"Lettuce",\n' +
+    '"Tomatoes",\n' +
+    '"Whole Grain Bread",\n' +
+    '"Ground Turkey",\n' +
+    '"Carrots",\n' +
+    '"Celery",\n' +
+    '"Onions",\n' +
+    '"Tomato Sauce",\n' +
+    '"Eggs",\n' +
+    '"Spinach",\n' +
+    '"Mushrooms",\n' +
+    '"Quinoa",\n' +
+    '"Cucumber",\n' +
+    '"Feta Cheese",\n' +
+    '"Lemon",\n' +
+    '"Lemon Juice",\n' +
+    '"Olive Oil",\n' +
+    '"Salt",\n' +
+    '"Pepper",\n' +
+    '"Salmon",\n' +
+    '"Asparagus",\n' +
+    '"Sweet Potatoes",\n' +
+    '"Greek Yogurt",\n' +
+    '"Berries",\n' +
+    '"Granola",\n' +
+    '"Sprouts",\n' +
+    '"Ciabatta",\n' +
+    '"Mozzarella",\n' +
+    '"Balsamic Vinegar",\n' +
+    '"Water",\n' +
+    '"BBQ Sauce",\n' +
+    '"Turkey",\n' +
+    '"Hummus",\n' +
+    '"Pesto",\n' +
+    '"Ciabatta Bread",\n' +
+    '"Beef",\n' +
+    '"Black Beans",\n' +
+    '"Corn",\n' +
+    '"Salsa",\n' +
+    '"Penne Pasta",\n' +
+    '"Meatballs",\n' +
+    '"Blueberries",\n' +
+    '"Maple Syrup"\n' +
+    ']\n' +
+    '}'
+}
+
 const app = express()
 const port = 3000
 const MODEL = 'mistralai/Mistral-7B-Instruct-v0.2'
@@ -23,7 +113,6 @@ app.listen(port, () => {
 })
 
 app.post('/menu', async (req, res) => {
-  //console.log('This is in the POST: ', req)
   const { title, body, userId } = req.body
   console.log(`Title: ${title}, Body: ${body}, UserId: ${userId}`);
   try {
@@ -45,6 +134,7 @@ app.post('/saveMenu', (req, res) => {
 })
 
 async function get_llm_response(data) {
+  /*
     //console.log("data:\n", data)
     //console.log("PROMPT+data: \n", PROMPT+data)
     const HF_TOKEN = process.env.HUGGINGFACE_TOKEN
@@ -56,6 +146,8 @@ async function get_llm_response(data) {
       });
     console.log("Out:\n", out.choices[0].message)
     return out.choices[0].message
+  */
+ return sampleResponse
 }
 
 function parseLLMResponse(LLMResponse) {
