16bf69b8a68ef342af5ba675b30ba7c11551a34d
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-08-30 17:18:35 +0200

    lost , in query...

--- src/db/sql/create_user_account_table.sql
+++ src/db/sql/create_user_account_table.sql
@@ -3,5 +3,5 @@ CREATE TABLE user_account (
     auth0_user_id VARCHAR(255) UNIQUE NOT NULL,
     nickname VARCHAR(255) NOT NULL UNIQUE,
     menuplan_id INT UNIQUE NOT NULL,
-    FOREIGN KEY (person_id) REFERENCES person(person_id) ON DELETE CASCADE,
+    FOREIGN KEY (person_id) REFERENCES person(person_id) ON DELETE CASCADE
 )
