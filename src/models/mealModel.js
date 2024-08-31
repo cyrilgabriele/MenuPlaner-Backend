51b4ff1f69fbcb110d1f86cece6cb60506dacd0f
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-08-31 18:52:30 +0200

    needed to add the day otherwise no clue to which day the meal corresponds to

--- src/models/mealModel.js
+++ src/models/mealModel.js
@@ -6,7 +6,8 @@ const mealModel = {
         for(const day in meals) {
             console.log(day)
             for(const meal_name in meals[day]) {
-                // console.log(meal_name)
+                const meal_name_concatenated = day+'_'+meal_name
+                console.log(meal_name)
                 const meal = meals[day][meal_name]
                 // console.log("meals[day][meal_name].title: ", meal.title)
                 // console.log("meals[day][meal_name].description: ", meal.description)
@@ -14,7 +15,7 @@ const mealModel = {
                 const meal_title = meal.title
                 // TODO: this call should happen in a controller (menu or menuplan?) but not here!
                 // here only do the data transformation
-                const meal_id = await saveMealQuery(meal_name, meal_description, menuplan_id, meal_title)
+                const meal_id = await saveMealQuery(meal_name_concatenated, meal_description, menuplan_id, meal_title)
                 meal_ids.push(meal_id)
             }
         }
