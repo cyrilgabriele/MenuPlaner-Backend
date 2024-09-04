8ce40eed6d2c284e2c061e95a0cc7d3a1322bf36
cyrilgabriele <GabrieleC@regatron.com>
2024-09-04 10:17:37 +0200

    removed UNIQUE constraint from nickname

--- src/db/sql/create_all_tables.sql
+++ src/db/sql/create_all_tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE person (
 CREATE TABLE user_account (
     person_id INT PRIMARY KEY, 
     auth0_user_id VARCHAR(255) UNIQUE NOT NULL,
-    nickname VARCHAR(255) UNIQUE NOT NULL,
+    nickname VARCHAR(255) NOT NULL,
     FOREIGN KEY (person_id) REFERENCES person(person_id) ON DELETE CASCADE
 );
 
