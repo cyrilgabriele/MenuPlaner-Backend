72607b30b62ae22868143056e0aa9d8cfff94f7a
cyrilgabriele <GabrieleC@regatron.com>
2024-09-04 10:08:03 +0200

    deleted unneccessary files

--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { authCallback } from '../controllers/authController.js'
-
-const router = express.Router()
-
-router.post('/callback', authCallback)
-
-export default router
