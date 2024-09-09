a8fb1f15f7c6b9032d7197c59fa470073c7f5eb1
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-09-09 21:22:28 +0200

    deleted the UNIQUE constraint of the auth0_id in the menuplan table

--- src/db/sql/create_all_tables.sql
+++ src/db/sql/create_all_tables.sql
@@ -14,7 +14,7 @@ CREATE TABLE user_account (
 
 CREATE TABLE menuplan (
     menuplan_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    auth0_user_id VARCHAR(255) UNIQUE NOT NULL, 
+    auth0_user_id VARCHAR(255) NOT NULL, 
     custom_prompt TEXT, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     FOREIGN KEY (auth0_user_id) REFERENCES user_account(auth0_user_id) ON DELETE CASCADE
